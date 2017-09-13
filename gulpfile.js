af1b0d8fdaad246966169e1668433b57fa0de6d5
oskfelin <oskfelin@gmail.com>
2017-09-13 14:52:20 +0200

    init commit; gulp; index.html

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'widget'
+        }
+    })
+});
+
+gulp.task('start', ['browserSync'], function (){
+    gulp.watch('widget/*.html', browserSync.reload);
+    gulp.watch('widget/style/*.css', browserSync.reload);
+});
