c639a847dddbfab2142108791d9bf33af0d892e2
oskfelin <oskfelin@gmail.com>
2017-09-14 12:43:07 +0200

    font imported

--- widget/scripts/title-loader.js
+++ widget/scripts/title-loader.js
@@ -5,11 +5,9 @@ function titleLoader() {
     $.getJSON("../data/songs.json", function (songs) {
         songs.forEach(function (song, index) {
             if (song.id === currId) {
-                $('#title').append(
-                    $('<div>')
+                $('#title')
                         .append($('<h3>').addClass('artist-name').text(song.artist))
                         .append($('<h5>').addClass('song-name').text(song.title))
-                );
             }
         })
     });
