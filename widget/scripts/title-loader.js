f7a1c28bf9d1777d5ea4120cb174712f45790f8c
oskfelin <oskfelin@gmail.com>
2017-09-14 15:30:28 +0200

    next button works

--- widget/scripts/title-loader.js
+++ widget/scripts/title-loader.js
@@ -1,15 +1,16 @@
+var currId = 1;
+
 function titleLoader() {
     $('#title').empty();
-    var currId = 1;
-
     $.getJSON("../data/songs.json", function (songs) {
         songs.forEach(function (song, index) {
             if (song.id === currId) {
                 $('#title')
                         .append($('<h3>').addClass('artist-name').text(song.artist))
-                        .append($('<h5>').addClass('song-name').text(song.title))
+                        .append($('<h5>').addClass('song-name').text(song.title));
+
             }
-        })
+        });
     });
 }
 
--- widget/scripts/track-controls.js
+++ widget/scripts/track-controls.js
@@ -1,13 +1,32 @@
 var $play = $('.play');
 var $pause = $('.pause');
-
+var $next = $('.next');
 $($play.hide());
 
-$($pause).click(function (event) {
+$($pause).click(function () {
     $(this).hide();
     $play.show()
 });
-$($play).click(function (event) {
+$($play).click(function () {
     $(this).hide();
     $pause.show()
 });
+
+$.getJSON("../data/songs.json", function (songs) {
+    $($next).click(function () {
+        if (currId <= songs.length - 1) {
+            currId = currId + 1;
+            titleLoader();
+            return currId + 1
+        }
+        else {
+            currId = 1;
+            titleLoader();
+            return currId
+        }
+    });
+});
+
+
+
+
