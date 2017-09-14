0c2aa5170e235129365d11c306aa6e81143866eb
oskfelin <oskfelin@gmail.com>
2017-09-15 00:23:39 +0200

    styling

--- widget/scripts/playlist.js
+++ widget/scripts/playlist.js
@@ -4,7 +4,7 @@ function playlist() {
     $('#songs-list').empty();
     $.getJSON("../data/songs.json", function (songs) {
         songs.forEach(function (song, index) {
-                $('#songs-list').append($('<div>')
+                $('#songs-list').append($('<div>').addClass('song')
                     .append($('<span>').addClass('song-duration').text(song.duration))
                     .append($('<span>').addClass('song-artist').text(song.artist))
                     .append($('<div>').addClass('song-name').text(song.title)))
